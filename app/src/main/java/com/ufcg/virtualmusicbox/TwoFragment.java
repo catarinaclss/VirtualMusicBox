a3c37a29d97e5f816dec2a584b87e3377cffb3e4
andregois <andrecg16@gmail.com>
2016-03-26 18:57:12 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/ufcg/virtualmusicbox/TwoFragment.java
@@ -0,0 +1,42 @@
+package com.ufcg.virtualmusicbox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view);
+        rv.setHasFixedSize(true);
+        MyAdapter adapter = new MyAdapter(new String[]{"Vem pro meu Lounge - Safadao", "Aquele 1% Vagabundo - Safadao", "Camarote - Safadao", "Sosseguei - Safadao",
+                "Voce Merece Cache - Safadao", "Sou ciumento mesmo - Safadao", "Novinha Vai no Chao - Safadao", "Na Hora da Raiva - Safadao", "Segunda Opcao - Safadao"});
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        return rootView;
+    }
+
+}
